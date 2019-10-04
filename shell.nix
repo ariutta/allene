f65ec306d5f2d74be4ca176bfbede1713ee6f0ab
Anders Riutta <git@andersriutta.com>
2019-10-04 13:41:23 -0700

    Everything works w/ both npm and yarn

--- shell.nix
+++ shell.nix
@@ -3,10 +3,5 @@ let
   deps = import ./mynixpkgs/environments/node.nix;
 in
   pkgs.mkShell {
-    buildInputs = deps ++ [
-      pkgs.libxml2 # for xmllint
-      pkgs.yarn
-      # what about yarn2nix?
-      #pkgs.yarn2nix
-    ] ++ (if stdenv.isDarwin then [] else []);
+    buildInputs = deps;
 }
